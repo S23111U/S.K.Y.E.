3c75f8297d11686de98a2715c3b199d0b5246efb
Swetanshu Patel <swetu23111@gmail.com>
2024-04-29 00:01:04 +0530

    Commit 29-04

--- GenAI.py
+++ GenAI.py
@@ -1,12 +1,17 @@
 import google.generativeai as genai
 import os
 from dotenv import load_dotenv
+
 load_dotenv()
+chatHistory = []
 
 
 def GenAI_search(prompt):
     GOOGLE_API_KEY = os.getenv('GOOGLE_API_KEY')
     genai.configure(api_key=GOOGLE_API_KEY)
     model = genai.GenerativeModel('gemini-pro')
-    ans = model.generate_content(prompt)
-    return ans.text
+
+    chatHistory.append({'role': 'user', 'parts': [prompt]})
+    response = model.generate_content(chatHistory)
+    chatHistory.append({'role': 'model', 'parts': [response.text]})
+    return response.text
--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+# import os
 import socket
 import webbrowser
 import re
 import wikipedia
+from fileOpen import open_whatsapp
+import threading
 
 # For web search
 from GenAI import GenAI_search
@@ -18,17 +21,23 @@ from weather import Get_Info
 # For alarm
 from set_alarm import set_alarm
 
-HOST = '192.168.1.21'
+# For reminder
+from set_reminder import set_reminder
+
+# Variable List
+HOST = '192.168.1.19'
 PORT = 12345
 youtube_pattern = re.compile(r'\bon youtube\b', re.IGNORECASE)
 wikipedia_pattern = re.compile(r'\bon wikipedia\b', re.IGNORECASE)
 sites = [["youtube", "https://youtube.com"], ["wikipedia", "https://wikipedia.com"], ["google", "https://google.com"],
-         ["spotify", "https://open.spotify.com"]]
-time, city, temp, sky, pos = Get_Info()
+         ["spotify", "https://open.spotify.com"], ["whatsapp", ""]]
+weather_time, city, temp, sky, pos = Get_Info()
+forAi = False
 
 
 def response_condition(speech):
-    # Entry------------------------------------------- 
+    global forAi
+    # Entry-------------------------------------------
     if f"Hey JARVIS".lower() in speech.lower():
         return Greetings() + "..."
     # -------------------------------------------
@@ -39,50 +48,85 @@ def response_condition(speech):
             webbrowser.open(site[1])
             return f"Opening {site[0]} sir" + "..."
 
-    # Feature 2: Searching on the internet
-    if f"search for".lower() in speech.lower():
+    # Feature 2: Tell the current time
+    if f"what's the time".lower() in speech.lower():
+        return "The time is, " + TellTime() + ", sir" + "..."
+
+    # Feature 3: Current weather conditions
+    if f"the weather".lower() in speech.lower():
+        return (f"The weather conditions in {city}  as of {weather_time} are, temperature is {temp}, sky is {sky} "
+                f"and wind is {pos} kilometer per hour" + "...")
+
+    # Feature 4: Set an alarm
+    if f"set an alarm".lower() in speech.lower():
+        pattern = r'\b\d{1,2}:\d{2}\s*[ap]\.m\.'
+        match = re.search(pattern, speech, re.IGNORECASE)
+        if match:
+            time = match.group(0)
+            time = time.replace(".", "").lower()
+            alarm_thread = threading.Thread(target=set_alarm, args=(time,))
+            alarm_thread.start()
+            return f"Alarm set for {time}, sir" + "..."
+        else:
+            return "Could not set an alarm, sir" + "..."
+
+    # Feature 5: Set a reminder
+    if f"remind me".lower() in speech.lower():
+        pattern = r'remind me to (.+?) at (\d+:\d+ [ap]\.m\.)'
+        match = re.search(pattern, speech, re.IGNORECASE)
+        if match:
+            task = match.group(1)
+            time = match.group(2)
+            time = time.replace(".", "").lower()
+            reminder_thread = threading.Thread(target=set_reminder, args=(time, task,))
+            reminder_thread.start()
+            return f"Reminder set to {task} at {time}, sir" + "..."
+        else:
+            return "Could not set a reminder, sir" + "..."
+
+    # Feature 6: Read out the news
+    if f"news".lower() in speech.lower():
+        pass
+
+    if f"open app".lower() in speech.lower():
+        # speech = speech.replace("open ", "")
+        open_whatsapp()
+        return "Opening whatsapp sir" + "..."
+
+    # Feature No.: Searching on the internet
+    if (f"search for".lower() in speech.lower()) or forAi:
         # On YOUTUBE
         if youtube_pattern.search(speech):
+            forAi = False
             speech = youtube_pattern.sub('', speech)
             speech = speech.replace("search for", "")
             youtube_link = "https://www.youtube.com/results?search_query=" + speech.strip()
             webbrowser.open(youtube_link)
             return "Opening sir" + "..."
-        # On the net
-        elif f"on the net".lower() in speech.lower():
-            speech = speech.replace("search for", "")
-            query = speech.replace("on the internet", "")
-            GenAI_result = GenAI_search(query)
-            return GenAI_result + "..."
+
         # On Wikipedia
         elif wikipedia_pattern.search(speech):
+            forAi = False
             speech = wikipedia_pattern.sub('', speech)
             speech = speech.replace("search for", "")
             result = wikipedia.summary(speech, sentences=2)
             return result + "..."
 
-    # Feature 3: Tell the current time
-    if f"what's the time".lower() in speech.lower():
-        return "The time is, " + TellTime() + ", sir" + "..."
+        # On the net
+        elif f"on the net".lower() in speech.lower() or forAi:
+            speech = speech.replace("search for", "")
+            query = speech.replace("on the net", "")
+            GenAI_result = GenAI_search(query)
+            forAi = True
+            return GenAI_result + "..."
 
-    # Feature 4: Current weather conditions
-    if f"the weather".lower() in speech.lower():
-        return (f"The weather conditions in {city}  as of {time} are, temperature is {temp}, sky is {sky} "
-                f"and wind is {pos} kilometer per hour" + "...")
+    else:
+        forAi = False
 
-    # Feature 5: Set an alarm
-    if f"set an alarm".lower() in speech.lower():
-        speech = speech.replace("Hey", "")
-        speech = speech.replace("JARVIS", "")
-        speech = speech.replace("for", "")
-        speech = speech.replace("set an alarm", "")
-        set_alarm(speech)
-        return f"Alarm set for {speech}, sir"
-
-    # Exit------------------------------------------- 
+    # Exit-------------------------------------------
     if f"That's it for now".lower() in speech.lower():
         return "I'll be glad to help you again, sir" + "..."
-    # ------------------------------------------- 
+    # -------------------------------------------
 
     return "Didn't understand, sir" + "..."
 
@@ -107,6 +151,6 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
             print(f"Received from client: {data.decode()}")
             data_received = data.decode()
 
-            #Send specific result based on the request
+            # Send specific result based on the request
             response = response_condition(data_received)
             conn.sendall(response.encode())
